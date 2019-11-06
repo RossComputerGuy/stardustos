4bc83c8a0053363bc25b7336924c187e1f1a5cec
Tristan Ross <spaceboyross@yandex.com>
2019-11-05 17:03:18 -0800

    [newland] scripts: fixed test

--- newland/scripts/test.sh
+++ newland/scripts/test.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-EMULATOR=$(shift)
-PROJECT_BINARY_DIR=$(shift)
+EMULATOR=$1
+PROJECT_BINARY_DIR=$2
+
+shift 2
 
 timeout 30s ${EMULATOR} -kernel ${PROJECT_BINARY_DIR}/kernel.elf -nographic -display none -serial stdio $@
 RETVAL=$?
