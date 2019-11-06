acbe255d1e7cb1dcd7b896887c9a1e2d4ad3d7fd
Tristan Ross <spaceboyross@gmail.com>
2019-11-05 18:59:28 -0800

    Update test.sh

--- newland/scripts/test.sh
+++ newland/scripts/test.sh
@@ -7,4 +7,4 @@ PROJECT_BINARY_DIR=$2
 
 shift 2
 
-$(dirname $0)/run.sh ${EMULATOR} ${PROJECT_BINARY_DIR} -nographic -display none $@ & sleep 30 ; kill $!
+$(dirname $0)/run.sh ${EMULATOR} ${PROJECT_BINARY_DIR} -nographic -display none $@ & sleep 10 ; kill $!
