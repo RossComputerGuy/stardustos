cc6900d57553ac155aadfdd8e40168fedfe68992
Tristan Ross <spaceboyross@gmail.com>
2019-11-05 18:35:38 -0800

    Update test.sh

--- newland/scripts/test.sh
+++ newland/scripts/test.sh
@@ -7,10 +7,4 @@ PROJECT_BINARY_DIR=$2
 
 shift 2
 
-timeout 30s $(dirname $0)/run.sh ${EMULATOR} ${PROJECT_BINARY_DIR} -nographic -display none $@
-RETVAL=$?
-if [ $RETVAL == 124 ]; then
-  exit 0
-else
-  exit $RETVAL
-fi
+$(dirname $0)/run.sh ${EMULATOR} ${PROJECT_BINARY_DIR} -nographic -display none $@ & sleep 30 ; kill $!
