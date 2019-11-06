c6519ffb1f7bdbbc0b2e5ae79c6baabf70f5eb4d
Tristan Ross <ross@bluelightos.ddns.net>
2019-11-06 17:50:23 +0000

    [newland] fixed modules

--- newland/module.cmake
+++ newland/module.cmake
@@ -12,5 +12,10 @@ include("${NEWLAND_SOURCE}/version.cmake")
 include("${NEWLAND_SOURCE}/arch/${NEWLAND_ARCH}/build.cmake")
 
 function(add_kmodule name sources)
-  # TODO: fixme
+  include_directories("${NEWLAND_SOURCE}/include" "${NEWLAND_SOURCE}/arch/${NEWLAND_ARCH}/include")
+  add_compile_definitions("NEWLAND_MODULE" "NEWLAND_VERSION=\"${NEWLAND_VERSION}\"")
+  add_compile_options("-nostdinc" "-fno-builtin" "-Werror=implicit-function-declaration" "-fno-stack-protector")
+  add_link_options("-T${NEWLAND_SOURCE}/arch/${NEWLAND_ARCH}/module.ld" "-nostdlib")
+  add_library("${name}" SHARED ${sources})
+  set_target_properties("${name}" PROPERTIES PREFIX "")
 endfunction()
