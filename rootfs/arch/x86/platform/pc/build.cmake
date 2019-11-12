920e8b39c49298da4f9afb94a64d7a705cb94082
Tristan Ross <spaceboyross@yandex.com>
2019-11-11 23:00:53 -0800

    [rootfs] x86: added product version and name

--- rootfs/arch/x86/platform/pc/build.cmake
+++ rootfs/arch/x86/platform/pc/build.cmake
@@ -1,5 +1,5 @@
 add_custom_command(OUTPUT "${PROJECT_BINARY_DIR}/rootfs.iso"
-  COMMAND grub-mkrescue -o "${PROJECT_BINARY_DIR}/rootfs.iso" "${PROJECT_BINARY_DIR}/rootfs"
+  COMMAND grub-mkrescue -o "${PROJECT_BINARY_DIR}/rootfs.iso" "${PROJECT_BINARY_DIR}/rootfs" --product-name=StardustOS --product-version="v0.1.0"
   COMMENT "Building ISO"
   MAIN_DEPENDENCY "${PROJECT_BINARY_DIR}/rootfs"
   DEPENDS "${PROJECT_BINARY_DIR}/rootfs/boot/kernel.elf" "${PROJECT_BINARY_DIR}/rootfs/boot/initrd.zip")
