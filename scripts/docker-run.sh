00c930e256bc7101277f58a355894d533b950483
Tristan Ross <spaceboyross@gmail.com>
2019-11-12 14:28:44 -0800

    Update docker-run.sh

--- scripts/docker-run.sh
+++ scripts/docker-run.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 if [ -n "$TRAVIS_BUILD_DIR" ]; then
- p="$TRAVIS_BUILD_DIR"
+  p="$TRAVIS_BUILD_DIR"
 else
-  p="$(dirname $0)"
-  p="$(dirname $p)"
+  p="$(dirname \"$0\")"
+  p="$(dirname \"$p\")"
 fi
 
-docker run -ti -v "$(readlink -f $p)":/usr/src -t stardustos /usr/src/scripts/build.sh
+docker run -ti -v "$(readlink -f \"$p\")":/usr/src -t stardustos /usr/src/scripts/build.sh
