197e7d54fdc8573700be7289b1f395337ffa1352
Tristan Ross <ross@bluelightos.ddns.net>
2019-11-18 17:38:42 +0000

    [nvk] adding unicorn

--- tools/nvk/include/nvk/proc.h
+++ tools/nvk/include/nvk/proc.h
@@ -7,16 +7,35 @@
 #include <stdint.h>
 #include <string.h>
 
+#define CHILD_MAX 128
+
+#define PROC_ZOMBIE 0
+#define PROC_FINISHED 1
+#define PROC_RUNNING 2
+#define PROC_READY 3
+
 typedef struct proc {
 	SLIST_ENTRY(struct proc) proc_list;
 	const char name[NAME_MAX];
 	const char tty[NAME_MAX];
 	const char cwd[PATH_MAX];
+	int status;
+	int exitval;
+	int isuser:1;
+	int issignaling:1;
 	pid_t id;
 	pthread_t thread;
 	gid_t gid;
 	uid_t uid;
+	pid_t parent;
+	pid_t child[CHILD_MAX];
+	size_t child_count;
+	void* (*entry)(void* impl);
+	void* impl;
 } proc_t;
 
 size_t process_count();
 proc_t* process_get(size_t i);
+
+proc_t* proc_create(proc_t* parent, const char* name, int isuser);
+void proc_go(proc_t** procptr);
--- tools/nvk/src/proc.c
+++ tools/nvk/src/proc.c
@@ -1,7 +1,9 @@
 /**
  * NewLand Virtual Kernel - (C) 2019 Tristan Ross
  */
+#include <newland/dev/tty.h>
 #include <nvk/proc.h>
+#include <errno.h>
 #include <pthread.h>
 #include <stdlib.h>
 
@@ -25,13 +27,61 @@ proc_t* process_get(size_t i) {
 	return NULL;
 }
 
+static void* proc_runner(void* arg) {
+	proc_t* proc = (proc_t*)arg;
+	proc->status = PROC_READY;
+	void* ret = NULL;
+	if (proc->isuser) {
+		// TODO: use unicorn to emulate
+	} else ret = (proc->entry == NULL ? NULL : proc->entry(proc->impl));
+	proc->status = PROC_ZOMBIE;
+	return ret;
+}
+
 proc_t* proc_create(proc_t* parent, const char* name, int isuser) {
 	proc_t* proc = malloc(sizeof(proc_t));
 	if (proc == NULL) return NULL;
 	memset(proc, 0, sizeof(proc_t));
 
+	int parent_index = -1;
+	if (parent != NULL) {
+		for (size_t i = 0; i < parent->child_count; i++) {
+			if (parent->child[i] == 0) {
+				parent_index = i;
+				break;
+			}
+		}
+		if (parent_index == -1) {
+			if (parent->child_count + 1 > CHILD_MAX) {
+				errno = -ECHILD;
+				return NULL;
+			}
+			parent_index = parent->child_count++;
+		}
+	}
+
 	strncpy((char*)proc->name, name, NAME_MAX);
 	proc->id = next_pid++;
+	proc->isuser = isuser;
+	proc->status = PROC_READY;
+
+	if (parent == NULL) {
+		strncpy((char*)proc->cwd, "/", PATH_MAX);
+		if (tty_get(0) != NULL) strncpy((char*)proc->tty, tty_get(0)->name, NAME_MAX);
+		proc->gid = proc->uid = proc->parent = 0;
+	} else {
+		strncpy((char*)proc->cwd, parent->cwd, PATH_MAX);
+		proc->gid = parent->gid;
+		proc->uid = parent->uid;
+		proc->parent = parent->id;
+		parent->child[parent_index] = proc->id;
+		strncpy((char*)proc->tty, parent->tty, NAME_MAX);
+	}
 	// TODO: finish this
 	return proc;
+}
+
+void proc_go(proc_t** procptr) {
+	proc_t* proc = *procptr;
+	pthread_create(&proc->thread, NULL, proc_runner, proc->impl);
 }
