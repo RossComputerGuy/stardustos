dd9b778b69672d86c40b3d67de41cb300331dab1
Tristan Ross <spaceboyross@yandex.com>
2019-11-17 21:39:06 -0800

    [tools] nvk: fixed

---
+++ tools/nvk/README.md
@@ -0,0 +1,3 @@
+# NVK
+
+NewLand Virtual Kernel is a program which emulates the NewLand kernel to provide a chroot-like program on a Linux host.
---
+++ tools/nvk/src/main.c
@@ -0,0 +1,37 @@
+/**
+ * NewLand Virtual Kernel - (C) 2019 Tristan Ross
+ */
+#include <argp.h>
+#include <stdlib.h>
+
+/** Argument parsing **/
+struct arguments {
+	char* rootfs;
+};
+
+const char* arg_program_version = "NVK 1.0";
+const char* argp_program_bug_address = "<spaceboyross@yandex.com>";
+static char doc[] = "NewLand Virtual Kernel, a program which emulates the NewLand kernel on Linux";
+static char args_doc[] = "ROOTFS";
+static struct argp_option options[] = {
+	{ 0 }
+};
+
+static error_t parse_opt(int key, char* arg, struct argp_state* state) {
+	struct arguments* args = state->input;
+	switch (key) {
+		case ARGP_KEY_ARG:
+			args->rootfs = arg;
+			break;
+		default: return ARGP_ERR_UNKNOWN;
+	}
+	return 0;
+}
+
+static struct argp argp = { options, parse_opt, args_doc, doc };
+
+int main(int argc, char** argv) {
+	struct arguments args;
+	argp_parse(&argp, argc, argv, 0, 0, &args);
+	return EXIT_SUCCESS;
+}
