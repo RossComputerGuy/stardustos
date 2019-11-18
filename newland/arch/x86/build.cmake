edf651edcb5f4bac9664f5ed0b262186270ebad2
Tristan Ross <ross@bluelightos.ddns.net>
2019-11-18 16:11:10 +0000

    [newland] x86: fixed for tools

--- newland/arch/x86/build.cmake
+++ newland/arch/x86/build.cmake
@@ -18,6 +18,8 @@ set(ARCH_FILES "${ARCH_DIR}/modules/bus/pci.c"
 	"${ARCH_DIR}/src/proc.c"
 	"${ARCH_DIR}/src/timer.c")
 include_directories("${ARCH_DIR}/include")
-add_compile_options("-m32")
-add_link_options("-m32")
+if(PROJECT_NAME STREQUAL "newland")
+	add_compile_options("-m32")
+	add_link_options("-m32")
+endif()
 set(EMULATOR i386)
