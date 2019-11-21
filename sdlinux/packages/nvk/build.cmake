30aa93a96ae2e44b4887c9ee4a7f99e1bdc9b270
Tristan Ross <ross@bluelightos.ddns.net>
2019-11-21 19:15:06 +0000

    [sdlinux] nvk: adding, kinda broken

---
+++ sdlinux/packages/nvk/build.cmake
@@ -0,0 +1,3 @@
+set(PACKAGE_OUTPUT ${PROJECT_BINARY_DIR}/initrd/bin/nvk)
+set(PACKAGE_DEPENDS ${PROJECT_BINARY_DIR}/initrd/lib/libunicorn.so)
+add_package(nvk 0.1.0)
--- tools/nvk/src/main.c
+++ tools/nvk/src/main.c
@@ -7,49 +7,15 @@
 #include <newland/module.h>
 #include <nvk/fs/rootfs.h>
 #include <nvk/proc.h>
-#include <argp.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <time.h>
 
 extern time_t boot_time;
 
-/** Argument parsing **/
-struct arguments {
-	char* rootfs;
-	char* initrd;
-};
-
-const char* arg_program_version = "NVK 1.0";
-const char* argp_program_bug_address = "<spaceboyross@yandex.com>";
-static char doc[] = "NewLand Virtual Kernel, a program which emulates the NewLand kernel on Linux";
-static char args_doc[] = "ROOTFS";
-static struct argp_option options[] = {
-	{ "initrd", 'r', "FILE", 0, "Uses FILE for the initrd" },
-	{ 0 }
-};
-
-static error_t parse_opt(int key, char* arg, struct argp_state* state) {
-	struct arguments* args = state->input;
-	switch (key) {
-		case 'r':
-			args->initrd = arg;
-			break;
-		case ARGP_KEY_ARG:
-			args->rootfs = arg;
-			break;
-		default: return ARGP_ERR_UNKNOWN;
-	}
-	return 0;
-}
-
-static struct argp argp = { options, parse_opt, args_doc, doc };
-
 int main(int argc, char** argv) {
-	struct arguments args = { NULL, NULL };
-	argp_parse(&argp, argc, argv, 0, 0, &args);
-
-	if (args.initrd == NULL && args.rootfs == NULL) {
-		fprintf(stderr, "Requires the initrd or rootfs argument\n");
+	if (argc < 1) {
+		fprintf(stderr, "Requires the rootfs argument\n");
 		return EXIT_FAILURE;
 	}
 
@@ -91,7 +57,7 @@ int main(int argc, char** argv) {
 		return EXIT_FAILURE;
 	}
 
-	r = mountpoint_create_fromnode(&fs, NULL, "/", 0, args.rootfs);
+	r = mountpoint_create_fromnode(&fs, NULL, "/", 0, argv[1]);
 	if (r < 0) {
 		fprintf(stderr, "Failed to find mount rootfs: %d\n", r);
 		return EXIT_FAILURE;
